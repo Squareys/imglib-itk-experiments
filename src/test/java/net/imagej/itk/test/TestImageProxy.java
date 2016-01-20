a939d9ffaa24c93202233f67557a1476bf343035
Squareys <squareys@googlemail.com>
2016-01-20 16:12:15 +0100

    Execute a sin filter in the Test
    Signed-off-by: Squareys <squareys@googlemail.com>

--- src/test/java/net/imagej/itk/test/TestImageProxy.java
+++ src/test/java/net/imagej/itk/test/TestImageProxy.java
@@ -1,18 +1,28 @@
 package net.imagej.itk.test;
 
+import org.itk.simple.Image;
+import org.itk.simple.SimpleITK;
 import org.junit.Test;
 
 import net.imagej.itk.ImageProxy;
 
 public class TestImageProxy {
-	
+
 	@Test
 	public void testImageProxy() {
 		long[] pixelData = new long[100];
-		long[] dims = new long[]{10, 10};
-		
+		long[] dims = new long[] { 10, 10 };
+
+		for (int i = 0; i < 100; ++i) {
+			pixelData[i] = i;
+		}
+
 		ImageProxy proxy = new ImageProxy(pixelData, dims);
-		
+		Image img = proxy.getImage();
+
+		Image result = SimpleITK.sin(img);
+
+		proxy.release();
 	}
 
 }
